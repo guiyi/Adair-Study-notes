5e71d37b17ccb77cebdd2b1f74329afa2890a67e
adair <zouhdai@gmail.com>
2017-02-21 21:42:28 +0800

    超全局变量

---
+++ super_variables.php
@@ -0,0 +1,24 @@
+<?php
+echo 'GLOBALS : '."<BR />";
+print_r($GLOBALS);
+echo "<BR /><BR />".'_SERVER : '."<BR />";
+print_r($_SERVER);
+echo "<BR /><BR />".'_GET : '."<BR />";
+print_r($_GET);
+echo "<BR /><BR />".'_POST : '."<BR />";
+print_r($_POST);
+echo "<BR /><BR />".'_COOKIE : '."<BR />";
+print_r($_COOKIE);
+echo "<BR /><BR />".'_FILES : '."<BR />";
+print_r($_FILES);
+echo "<BR /><BR />".'_ENV : '."<BR />";
+print_r($_ENV);
+echo "<BR /><BR />".'_REQUEST : '."<BR />";
+print_r($_REQUEST);
+echo "<BR /><BR />".'_SESSION : '."<BR />";
+print_r($_SESSION);
+//var_dump($_ENV);
+
+
+
+?>
