40c54ca8a0f72b47e34f1a8bc173b3813f5089e6
adair <zouhdai@gmail.com>
2019-04-22 21:33:48 +0800

    无刷新上传图片

--- image.php
+++ image.php
@@ -12,14 +12,14 @@
         exit(json_encode(array('code'=>1,'msg'=>'上传图片不合法')));
     }
 
-    if (file_exists("/tmp/" . $_FILES["file"]["name"])){
+    if (file_exists("/data/wwwroot/3.0.1.2-opencart/images/" . $_FILES["file"]["name"])){
       	//echo $_FILES["file"]["name"] . " already exists. ";
     }else{
-      	$info = move_uploaded_file($_FILES["file"]["tmp_name"],"/tmp/" . $_FILES["file"]["name"]);
+      	$info = move_uploaded_file($_FILES["file"]["tmp_name"],"/data/wwwroot/3.0.1.2-opencart/images/" . $_FILES["file"]["name"]);
       	//echo "Stored in: " . "/tmp/" . $_FILES["file"]["name"];
     }
     
-    $img = "/tmp/" . $_FILES["file"]["name"];
+    $img = "/images/" . $_FILES["file"]["name"];
     exit(json_encode(array('code'=>0,'msg'=>$img)));
  
 
@@ -33,7 +33,7 @@
 <div class="layui-form-item">
     <label class="layui-form-label">图片</label>
     <div class="layui-input-block">
-        <img id="pre_img" style="height: 30px;" />
+        <img id="pre_img" style="width: 100px;height: 100px;" />
         <button class="layui-btn layui-btn-sm" onclick="return false;" id="upload_img">
             <i class="layui-icon">&#xe67c;</i>上传图片
         </button>
